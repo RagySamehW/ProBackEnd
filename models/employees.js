0c216fe408383c7bd119176c3744f3f38bcd7057
RagySamehW <ragy2106213@miuegypt.edu.eg>
2023-05-19 01:16:49 +0300

    DB

---
+++ models/employees.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const employeeSchema = new Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  Email: {
+    type: String,
+    required: true
+  },
+  Phone:{
+    type : Number,
+    required: true
+  },
+  Type: {
+    type: String,
+    required: true
+  },
+}, { timestamps: true });
+
+const Employees = mongoose.model('Employees', employeeSchema);
+module.exports = Employees;
