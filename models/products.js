77f19e6f5fd71300056a539b3a6f85cda98b7c76
Mostafaaelbarbary <126014488+Mostafaaelbarbary@users.noreply.github.com>
2023-05-27 00:58:16 +0300

    Update products.js
    update products

---
+++ models/products.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+  ProductName: {
+    type: String,
+    required: true,
+  },
+  Price: {
+    type: Number,
+    
+  },
+  Size: {
+    type: String,
+    required: true
+  },
+  Quantity:{
+    type : Number,
+  },
+  Description: {
+    type: String,
+  },
+  Image:{
+    type: String,
+  },
+}, { timestamps: true });
+
+const products = mongoose.model('Product', productSchema);
+module.exports = products;
