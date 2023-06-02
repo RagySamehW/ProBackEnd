3a3e98e9c616b2377cef925095233b1a86206351
RagySamehW <ragy2106213@miuegypt.edu.eg>
2023-06-03 02:26:43 +0300

    .

--- routes/home.js
+++ routes/home.js
@@ -1,42 +1,79 @@
 const express = require("express");
 const Product = require("../models/products");
+const { await } = require("await");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.render("Home", {
-      user: req.session.user === undefined ? "" : req.session.user,
- });
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.aggregate([{ $sample: { size: 8 } }]).exec();
+    const productss = await Product.aggregate([{ $sample: { size: 4 } }]).exec();
+    res.render('Home', { productss ,products ,user: req.session.user === undefined ? "" : req.session.user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+router.get("/Contact", (req, res) => {
+    res.render('Contact', { user: req.session.user === undefined ? "" : req.session.user });
   });
 
-  router.get("/Contact", (req, res) => {
-    res.render("Contact", {
-      user: req.session.user === undefined ? "" : req.session.user,
-    });
-  });
 
-  router.get("/Product", (req, res) => {
-    var products;
-    Product.find().then((data) => {
-      products = data;
+ 
+
+const itemsPerPage = 4;
+  router.get("/Product", async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+  
+    try {
+      // Count the total number of products in the database
+      const totalProductsCount = await Product.countDocuments();
+  
+      // Calculate the start and end indexes of the products to display
+      const startIndex = (page - 1) * itemsPerPage;
+      const endIndex = page * itemsPerPage;
+  
+      // Fetch the paginated products from the database
+      const paginatedProducts = await Product.find()
+        .skip(startIndex)
+        .limit(itemsPerPage);
+  
+      // Render the products page template with the paginated products and page information
       res.render("ProductsPage", {
+        products: paginatedProducts,
+        currentPage: page,
+        totalPages: Math.ceil(totalProductsCount / itemsPerPage),
+        user: req.session.user === undefined ? "" : req.session.user,
+      });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      // Handle the error and send an appropriate response
+      res.status(500).send("Error fetching products");
+    }
+  });
+
+router.post("/Item", async (req, res) => {
+  var products;
+  const productss = await Product.aggregate([{ $sample: { size: 4 } }]).exec();
+  Product.find({ ProductName: req.body.pname, Size: req.body.psize }).then(
+    (data) => {
+      products = data;
+      console.log(products);
+      res.render("Item", {
+        productss,
         products,
         user: req.session.user === undefined ? "" : req.session.user,
       });
-     })
-  });
+    }
+  );
+});
+router.post("/getproducts", async (req, res) => {
+  let payload = req.body.payload.trim();
+  let search = await Product.find({
+    ProductNam: { $regex: new RegExp("^" + payload + ".*", "i") },
+  }).exec();
+  res.send({ payload: search });
+});
 
-  router.post("/Item", (req, res) => {
-    var products;
-    Product.find({ ProductName: req.body.pname, Size: req.body.psize }).then(
-      (data) => {
-        products = data;
-        console.log(products);
-        res.render("Item", {
-          products,
-          user: req.session.user === undefined ? "" : req.session.user,
-        });
-      }
-    );
-  });
-
-  module.exports = router;
+module.exports = router;
