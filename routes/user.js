933ce4e61e40ffc77bddc037b2f690de9640ee52
youssef sherin <125735335+sherry259@users.noreply.github.com>
2023-06-03 15:10:06 -0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -19,23 +19,9 @@ router.get("/signup", (req, res) => {
   });
 });
 
-router.post('/WishList', (req, res) => {
-  // Retrieve the product ID from the request body
-  const productId = req.body.productId;
 
-  // Perform your logic to add/remove the product from the user's favorites list
-  // Example: Save the product ID in the user's favorites collection in the database
-
-  // Send the response back to the client
-  res.sendStatus(200); // Sending a success status code (e.g., 200) to indicate successful operation
-});
-
-router.get("/WishList", (req, res) => {
-  res.render("WishList", {
-    custemors,
-    user: req.session.user === undefined ? "" : req.session.user,
-  });
-});
+router.get('/wishlist/remove/:productId', user.removeFromWishlist);
+router.get('/wishlist/AddtoCart/:productId', user.AddtoCart);
 
 router.get("/Cart", User.Cartv);
 router.post("/profile", User.Getuser);
@@ -56,7 +42,7 @@ router.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
-router.get('/wishlist/AddtoCart/:productId', user.AddtoCart);
+
 
 router.post("/add_to_cart", User.AddtoCart);
 router.post("/remove_from_cart", User.RemoveCart);
