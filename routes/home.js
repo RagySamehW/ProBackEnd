26f4d8bf5c2aadcf2b48951e88202cbb952bc35b
RagySamehW <ragy2106213@miuegypt.edu.eg>
2023-06-05 01:55:20 +0300

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -76,7 +76,4 @@ router.post("/getproducts", async (req, res) => {
   res.send({ payload: search });
 });
 
-router.get('/WishList', (req, res) =>{
-  res.render('WishList',{ employees});
-});
 module.exports = router;
