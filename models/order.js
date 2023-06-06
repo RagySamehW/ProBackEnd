178322ba2cd408dd60a293578a3ab9aa6e8d5f7a
youssef sherin <125735335+sherry259@users.noreply.github.com>
2023-06-06 09:04:25 -0700

    Create order.js

---
+++ models/order.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import User from "../models/employees.js";
+
+const orderSchema = new mongoose.Schema({
+    UserId: String,
+
+    item:[{ productId: String,
+    productName: String,
+    productPrice: Number,
+    quantity:{ 
+      type: Number ,
+      default: 0
+     },
+  } ],
+     totalPrice:{
+      type: Number,
+      default: 0
+     },
+    
+     address:{
+        type: String,
+        required: true,
+        
+     },
+      address2:{
+        type: String,
+        required: true,
+        
+      },
+      additionaladd:{
+        type: String,
+        required: false,
+
+      },
+
+      city:{
+        type: String,
+        required: true,
+      },
+       phone:{
+        type: String,
+        required: true,
+       }
+  });
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
